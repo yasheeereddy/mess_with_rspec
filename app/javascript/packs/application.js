4c8410e5e5584d907129a428d7201ac40b6562e3
yasheeereddy <yaswitha.buchi@zaggle.in>
2022-09-02 17:06:18 +0530

    thirdcommit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("semantic-ui-sass")
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
