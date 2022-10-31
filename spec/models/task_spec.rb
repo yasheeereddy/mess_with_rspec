deb4a1e6cb4d7e4afd7bdc227cff5c0f15cee7f7
yasheeereddy <yaswitha.buchi@zaggle.in>
2022-10-31 13:32:59 +0530

    added traits1

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,11 @@ RSpec.describe Task, type: :model do
   context "Validations" do
     subject { Task.new(name:"eat food", status:"true")}
     it "is not valid without a name" do
+      subject.name= nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a status" do
+      subject.status=nil
       expect(subject).to_not be_valid 
     end  
     it "is valid with a unique name" do
